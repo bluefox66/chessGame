64a9e3b382cf18226728570c7d60a8e855842023
Kamil Dralewski <kamildralewski@interia.pl>
2021-12-21 21:39:41 +0100

    sposoby zapisu planszy

---
+++ test.php
@@ -0,0 +1,48 @@
+$chessboard = array(
+    1 => array(
+        'a' => array(
+            'piece' => 'rock',
+            'color' => 'white',
+        ),
+        'b' =>array(
+            'piece' => 'knight',
+            'color' => 'white',
+        ),
+    ),
+    2 => array(
+        'a' => array(
+            'piece' => 'pawn',
+            'color' => 'white',
+        ),
+        'b' =>array(
+            'piece' => 'pawn',
+            'color' => 'white',
+        ),
+    ),
+);
+
+//figura na b3
+$x = $chessboard[3]['b'];
+
+$chessboard2 = array(
+    a1 => array(
+        'a1' => array(
+            'piece' => 'rock',
+            'color' => 'white',
+        ),
+        'a2' =>array(
+            'piece' => 'knight',
+            'color' => 'white',
+        ),
+    'b1' => array(
+        'b1' => array(
+            'piece' => 'pawn',
+            'color' => 'white',
+        ),
+        'b2' => array(
+            'piece' => 'pawn',
+            'color' => 'white',
+        ),
+        
+    );
+    $x = $chessboard2['b3'];
